d2719878b2dd9418d5ac698e66c05729c9d5fb7a
yamaksjd <luyanavunge@gmail.com>
2025-03-17 20:26:03 +0000

    commet to solve error

--- scope/scope_lab.js
+++ scope/scope_lab.js
@@ -15,3 +15,7 @@ const blockConst = "I'm a block-scoped const";
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+//Block Scope
+console.log(blockVar);
+console.log(blockLet);
