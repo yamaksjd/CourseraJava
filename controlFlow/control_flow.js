e9324f3d72614776f923b3656af12d4adbca635d
yamaksjd <luyanavunge@gmail.com>
2025-03-30 20:35:52 +0100

    user Message control flow

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -9,4 +9,19 @@ if (userRole === "admin") {
     accesLevel = "No access granted";
 }
 
-console.log("Access Level", accessLevel);
+console.log("Access Level: ", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn) {
+    if(userRole === "admin") {
+        userMessage = "Welcome, Admin";
+    } else  {
+        userMessage = "Welcome, User";
+    }
+} else {
+    userMessage = "Please log in to access the system"
+}
+
+console.log("User Message: ", userMessage)
