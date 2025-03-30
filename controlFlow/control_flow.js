457933045dd2664227b0493a6668822b18cb2f4a
yamaksjd <luyanavunge@gmail.com>
2025-03-30 20:27:06 +0100

    control flow lab

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,12 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accesLevel = "No access granted";
+}
+
+console.log("Access Level", accessLevel);
--- scope/scope_lab.js
+++ scope/scope_lab.js
@@ -17,5 +17,5 @@ console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
-console.log(blockVar);
-console.log(blockLet);
+//console.log(blockVar);
+//console.log(blockLet);
