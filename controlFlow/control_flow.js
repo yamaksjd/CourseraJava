b42336a3c4aada1e444b772229a84decda5d76ae
yamaksjd <luyanavunge@gmail.com>
2025-03-30 20:41:51 +0100

    user category

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -25,3 +25,22 @@ if(isLoggedIn) {
 }
 
 console.log("User Message: ", userMessage)
+
+let userType = "subscriber";
+let userCategory;
+
+switch(userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown"
+}
+
+console.log("User Category: ", userCategory);
