a5423afa9943e8ee258572e28b5dd6930a98ce61
yamaksjd <luyanavunge@gmail.com>
2025-04-01 21:00:24 +0100

    lab 4 product survey

---
+++ productSurvey/productSurvey.js
@@ -0,0 +1,28 @@
+const submitBtn = document.getElementById("submitBtn");
+const username = document.getElementById("name").value;
+const age = document.getElementById("age").value;
+const email = document.getElementById("email").value;
+const job = document.getElementById("job").value;
+const designation = document.getElementById("designation").value;
+const productType = document.getElementById("ProductType").value;
+const feedback = document.getElementById("feedbackText").textContent;
+
+
+submitBtn.onclick = submitFeedback();
+    function submitFeedback() {
+        alert("Thank you for your valuable feedback");
+        document.getElementById("userName").innerHTML = username;
+        document.getElementById("userAge").innerHTML = age;
+        document.getElementById("userEmail").innerHTML = email;
+        document.getElementById("userJob").innerHTML = job;
+        document.getElementById("userDesignation").innerHTML = designation;
+        document.getElementById("userProductChoice").innerHTML = productType;
+        document.getElementById("userFeedback").innerHTML = feedback;
+        document.getElementById("userInfo").style.display = "block"
+    }
+
+document.addEventListener("keydown", e => {
+    if(e.key === "Enter") {
+        submitFeedback();
+    }
+});
